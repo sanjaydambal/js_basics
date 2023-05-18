3023265a84e2c904f2184760dd4724d1980889c4
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-05-18 22:11:10 +0530

    rest/spread of objects

--- closure.js
+++ closure.js
@@ -21,3 +21,17 @@ const pdt = function(x,y,z){
 const cal = pdt(5,8,6);
 console.log(cal.getPdtTwo());
 console.log(cal.getPdtThree());
+
+const student = {
+    name:"sanjay",
+    age:34,
+    profession:"Lecturer"
+}
+const {...rest} = student;
+console.log(rest);
+
+const student2 ={
+    ...student,
+    age:35
+}
+console.log(student2);
