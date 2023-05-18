f4286d1c07f82e5121022a6cb32147a3fd7eda30
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-05-18 21:36:58 +0530

    closure ex2

--- closure.js
+++ closure.js
@@ -7,3 +7,17 @@ return a+b+c;
 }
 const store = sum(2);
 console.log(store(10));
+
+const pdt = function(x,y,z){
+    return{
+        getPdtTwo:function(){
+            return x*y;
+        },
+        getPdtThree:function(){
+            return x*y*z;
+        }
+    }
+}
+const cal = pdt(5,8,6);
+console.log(cal.getPdtTwo());
+console.log(cal.getPdtThree());
