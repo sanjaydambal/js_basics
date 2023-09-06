f40e98ac95245f7c2c6075e73168940d19f85732
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-09-06 15:35:21 +0530

    Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.

--- leetcode.js
+++ leetcode.js
@@ -8,3 +8,11 @@ var filter = function(arr, fn) {
     }
     return filteredArr;
 };
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+var map = function(arr, fn) {
+    const arr2 = [];
+    for(let i=0;i<arr.length;i++){
+        arr2[i]= fn(arr[i],i)
+    }
+    return arr2;
+};
