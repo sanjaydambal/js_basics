1e0b0844e7b7efe3280a00b498c512248b935fc5
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-09-20 23:14:18 +0530

    combining two tables

--- leetcode.sql
+++ leetcode.sql
@@ -14,3 +14,32 @@ Write a solution to find the second highest salary from the Employee table. If t
 SELECT MAX(salary) AS second_highest_salary
 FROM Employee
 WHERE salary < (SELECT MAX(salary) FROM Employee);
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| personId    | int     |
+| lastName    | varchar |
+| firstName   | varchar |
++-------------+---------+
+personId is the primary key (column with unique values) for this table.
+This table contains information about the ID of some persons and their first and last names.
+ 
+
+Table: Address
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| addressId   | int     |
+| personId    | int     |
+| city        | varchar |
+| state       | varchar |
++-------------+---------+
+addressId is the primary key (column with unique values) for this table.
+Each row of this table contains information about the city and state of one person with ID = PersonId.
+ 
+
+Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+select firstName ,lastName,city,state from Person left outer join address on person.personId=address.personId;
