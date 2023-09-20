80076ac621032d9f2327251456cc06ca47474130
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-09-20 23:11:36 +0530

    Max salary

---
+++ leetcode.sql
@@ -0,0 +1,16 @@
+-- Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest salary from the Employee table. If there is no second highest salary, return null
+SELECT MAX(salary) AS second_highest_salary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
